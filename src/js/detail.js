279275740d20fa52db142ebedfc15a91a59efaf6
wangyan <wangyan@qq.com>
2018-06-29 01:09:09 +0800

    放大镜

--- src/js/detail.js
+++ src/js/detail.js
@@ -1,10 +1,18 @@
-require(["config"], function () {
-    require(["jquery", "load","zoom"], function ($) {
-		$('.zoom').elevateZoom({
-			gallery:'gallery_01',
-			cursor : 'pointer',
-			galleryActiveClass : 'active'
+require(["config"], function() {
+	require(["jquery", "load", "zoom"], function($) {
+		$(".zoom").elevateZoom({
+			zoomType: "inner",
+			gallery: 'gallery_01',
+			debug: true,
+			cursor: "pointer",
+			zoomWindowFadeIn: 500,
+			zoomWindowFadeOut: 500
 		});
-
+		$(".len_bottom").on("mouseenter",".active",function(){
+			$(this).css({"border-color":"red"});
+		})
+		$(".len_bottom").on("mouseleave",".active",function(){
+			$(this).css({"border-color":"#d6d6d6"});
+		})
 	})
 })
